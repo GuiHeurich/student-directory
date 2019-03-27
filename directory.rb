b7089656aeb1fb685eb663f2d386b071a680499f
Guilherme <guiheurich@gmail.com>
2019-03-27 10:16:19 +0000

    Corrected typos in directory

--- directory.rb
+++ directory.rb
@@ -58,7 +58,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list"
-  puts "9. Exit" # 9 because we'll be adding more items
+  puts "9. Exit"
 end
 
 def show_students
@@ -83,7 +83,7 @@ def process(selection)
 end
 
 def save_students
-  #open the file for writin
+  #open the file for writing
   file = File.open("students.csv", "w")
   #iterate over the array of students
   @students.each do |student|
