c09b4a569e05e8b9e33b43a544a62c1f467b83f6
Guilherme <guiheurich@gmail.com>
2019-03-28 09:51:06 +0000

    Changed try_load to load from file

--- directory.rb
+++ directory.rb
@@ -107,15 +107,15 @@ def load_students(filename = "students.csv")
 end
 
 def try_load_students
-  filename = ARGV.first #first argument from the command line
-  return if filename.nil? #get out of the method if it isn't given
-  if File.exists?(filename) #if it exists
-    load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
-  else #if it doesn't exist
-    puts "Sorry #{filename} doesn't exist."
-    exit
-  end
+  #filename = ARGV.first #first argument from the command line
+  #return if filename.nil? #get out of the method if it isn't given
+  #if File.exists?(filename) #if it exists
+    load_students("students.csv")
+    puts "Loaded #{@students.count} from students.csv"
+ #  else #if it doesn't exist
+#   puts "Sorry #{filename} doesn't exist."
+ #   exit
+ # end
 end
 
 def add_student(name, cohort)
