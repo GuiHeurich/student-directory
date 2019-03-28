429669878d9b7b0d458ba0a0c82ff1d5ed33cc0e
Guilherme <guiheurich@gmail.com>
2019-03-28 15:22:38 +0000

    Added CSV usage

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+require 'csv'
 @students = []
 
 def try_load_students
@@ -84,24 +85,20 @@ end
 def save_students
   puts "Tell me in which file to save:"
   filename = gets.chomp
-  file = File.open(filename, "w") do |file|
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
+  CSV.open(filename, "w") do |csv|
+    @students.each do |student|
+      csv << [student[:name], student[:cohort]]
+    end
   puts "\nSaved #{@students.count} students to #{filename}!\n".center(40)
   end
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r") do |file|
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(",")
+  CSV.foreach(filename) do |row|
+    name, cohort = row
     add_student(name, cohort)
-  end
+    end
   puts "Sucessfully loaded #{@students.count} from #{filename}".center(60)
-  end
 end
 
 try_load_students
